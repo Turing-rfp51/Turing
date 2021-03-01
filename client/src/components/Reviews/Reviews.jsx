59ab65ab64a4bc30cd2d6afcb6390d7b76e06ea3
Austin Elwell <austinelwell99@gmail.com>
2021-03-01 12:46:24 -0600

    Refactored to class component, set up state to hold reviews

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -1,24 +1,44 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/prefer-stateless-function */
 /* eslint-disable func-names */
 import React from 'react';
 import axios from 'axios';
 
 const { TOKEN } = require('../../../../config.js');
 
-const productId = 17762;
-const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews?product_id=${productId}`;
-const config = {
-  headers: { Authorization: TOKEN },
-};
+const productId = 17762; // will likely come in as props. Only hardcoded for now
 
-const initializeReivews = function () {
-  axios
-    .get(url, config)
-    .then((data) => console.log(data))
-    .catch((err) => console.error(err));
-};
+class Reviews extends React.Component {
+  constructor(props) {
+    super(props);
 
-initializeReivews();
+    this.state = {
+      reviews: []
+    }
+  }
 
-const Reviews = () => <div>Reviews</div>;
+  componentDidMount() {
+    this.initializeReivews()
+  }
+
+  initializeReivews() {
+    axios
+      .get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews?product_id=${productId}`, { headers: { Authorization: TOKEN } })
+      .then((obj) => {
+        this.setState({reviews: obj.data.results})
+        console.log(obj)
+      })
+      .catch((err) => console.error(err));
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.reviews.map(r => <div key={r.review_id}>{r.summary}</div>)}
+      </div>
+    )
+  }
+}
 
 export default Reviews;
