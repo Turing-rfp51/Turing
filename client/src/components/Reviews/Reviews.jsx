009e8a51171ed899c4f7450a5845bed5f4ba441b
Austin Elwell <austinelwell99@gmail.com>
2021-03-01 12:29:53 -0600

    Initalized API call for reviews

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -1,5 +1,24 @@
+/* eslint-disable func-names */
 import React from 'react';
+import axios from 'axios';
 
-const Reviews = () => <div>Reviews Component</div>;
+const { TOKEN } = require('../../../../config.js');
+
+const productId = 17762;
+const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews?product_id=${productId}`;
+const config = {
+  headers: { Authorization: TOKEN },
+};
+
+const initializeReivews = function () {
+  axios
+    .get(url, config)
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
+};
+
+initializeReivews();
+
+const Reviews = () => <div>Reviews</div>;
 
 export default Reviews;
