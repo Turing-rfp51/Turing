8183fdb7bd70476aa2d6e40c985c2f19b72422c5
Austin Elwell <austinelwell99@gmail.com>
2021-03-02 15:37:29 -0600

    Moved buttons out of scrollbox, added new review button and click handler

--- client/src/components/Reviews/ReviewsList.jsx
+++ client/src/components/Reviews/ReviewsList.jsx
@@ -4,13 +4,17 @@ import ReviewListItem from './ReviewListItem.jsx';
 import ReviewsSortDropdown from './ReviewsSortDropdown.jsx';
 import ReviewsListButtons from './ReviewsListButtons.jsx';
 
-const ReviewsList = ({ reviews, updateSortBy, showMoreReviews }) => (
+const ReviewsList = ({ reviews, updateSortBy, showMoreReviews, addNewReview }) => (
   <div className='reviewListContainer'>
     <ReviewsSortDropdown updateSortBy={updateSortBy} />
     <div className='reviewListDisplay'>
       {reviews.length > 0 && reviews.map((r) => <ReviewListItem review={r} key={r.review_id} />)}
-      <ReviewsListButtons showMoreReviews={showMoreReviews} />
     </div>
+    <ReviewsListButtons
+      showMoreReviews={showMoreReviews}
+      numOfReviews={reviews.length}
+      addNewReview={addNewReview}
+    />
   </div>
 );
 
