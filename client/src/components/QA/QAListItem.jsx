5b0c290bb4c6476a707e789b7ddd516be150275d
Irene Hodge <irenehodge90@gmail.com>
2021-03-02 15:34:58 -0800

    Answers now rendering on th page

--- client/src/components/QA/QAListItem.jsx
+++ client/src/components/QA/QAListItem.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 import QAHeader from './QAHeader.jsx';
 import Question from './Question.jsx';
-import Answer from './Answer.jsx';
+import AnswersList from './AnswersList.jsx';
 import HelpfulUpvote from './HelpfulUpvote.jsx';
 import AddAnswer from './AddAnswer.jsx';
 import QAFooter from './QAFooter.jsx';
@@ -11,7 +12,7 @@ const QAListItem = ({ questionData }) => (
   <div className='QAListItem'>
     <QAHeader />
     <Question questionData={questionData} />
-    <Answer questionData={questionData} />
+    <AnswersList answers={questionData.answers} />
     <HelpfulUpvote />
     <AddAnswer />
     <QAFooter />
