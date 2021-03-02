624fde713844398d1563bf52ebd934ead66fee0f
Irene Hodge <irenehodge90@gmail.com>
2021-03-02 13:33:40 -0800

    Updated QA component to get data from API

--- client/src/components/QA/QA.jsx
+++ client/src/components/QA/QA.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
 import axios from 'axios';
 
+import QuestionsList from './QuestionsList.jsx';
+
 const { TOKEN } = require('../../../../config.js');
 
-const QA = () => <h1>Hello World!</h1>;
+class QA extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    this.initializeQA();
+  }
+
+  initializeQA() {
+    axios
+      .get(
+        `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/questions?product_id=${this.props.productId}`,
+        { headers: { Authorization: TOKEN } }
+      )
+      .then((obj) => {
+        this.setState({ data: obj.data });
+        console.log(obj.data);
+      })
+      .catch((err) => console.error(err));
+  }
+
+  render() {
+    return (
+      <div className='qaModuleContainer'>
+        <QuestionsList data={this.state.data} />
+      </div>
+    );
+  }
+}
 
 export default QA;
