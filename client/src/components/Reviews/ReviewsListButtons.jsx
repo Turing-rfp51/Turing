d83fd0267fd717905a91f2fed95763c854ef161b
Austin Elwell <austinelwell99@gmail.com>
2021-03-02 15:03:31 -0600

    Added show more reviews functionality, including scrollbox

---
+++ client/src/components/Reviews/ReviewsListButtons.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const ReviewsListButtons = ({ showMoreReviews }) => (
+  <button type='button' onClick={showMoreReviews}>
+    Show More Reviews
+  </button>
+);
+
+export default ReviewsListButtons;
