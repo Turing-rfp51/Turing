77b5d731cd16ac1a68715803e71c61dda1dfef93
Austin Elwell <austinelwell99@gmail.com>
2021-03-02 17:03:14 -0600

    Added expand review body functionality

--- client/src/components/Reviews/ReviewBody.jsx
+++ client/src/components/Reviews/ReviewBody.jsx
@@ -1,6 +1,32 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-const ReviewBody = ({ review }) => <p>{review.body}</p>;
+class ReviewBody extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: false,
+    };
+  }
+
+  render() {
+    const { review } = this.props;
+    const { expanded } = this.state;
+    return (
+      <React.Fragment>
+        {(review.body.length <= 250 || expanded) && <p>{review.body}</p>}
+        {review.body.length > 250 && !expanded && (
+          <React.Fragment>
+            <p>{review.body.slice(0, 250)}</p>
+            <button type='button' onClick={() => this.setState({ expanded: true })}>
+              Show More
+            </button>
+          </React.Fragment>
+        )}
+      </React.Fragment>
+    );
+  }
+}
 
 export default ReviewBody;
