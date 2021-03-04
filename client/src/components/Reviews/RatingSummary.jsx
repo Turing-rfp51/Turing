0c07732c64442479ef2eec017cfa4c783e1e0a3e
Austin Elwell <austinelwell99@gmail.com>
2021-03-03 18:59:18 -0600

    Additional styling and using new stars on individual reviews

--- client/src/components/Reviews/RatingSummary.jsx
+++ client/src/components/Reviews/RatingSummary.jsx
@@ -2,6 +2,13 @@
 import React from 'react';
 import AverageStarRating from '../Shared/AverageStarRating.jsx';
 
-const RatingSummary = ({ metadata }) => <AverageStarRating metadata={metadata} />;
+const RatingSummary = ({ metadata }) => (
+  <div className='ratingSummaryContainer'>
+    <AverageStarRating metadata={metadata} />
+    <div className='totalReviewCountText'>
+      {+(metadata.recommended.true || 0) + +(metadata.recommended.false || 0)} Reviews
+    </div>
+  </div>
+);
 
 export default RatingSummary;
