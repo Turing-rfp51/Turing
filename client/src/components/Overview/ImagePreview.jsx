86af792e7f9b8b94811160026b18cd662afd7910
tillmattson <tillmattson@gmail.com>
2021-03-08 18:23:54 -0700

    Fixed stuff

--- client/src/components/Overview/ImagePreview.jsx
+++ client/src/components/Overview/ImagePreview.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unused-state */
 import React from 'react';
-import ImageModal from './ImageModal.jsx';
-import ImageViewBox from './ImageViewBox.jsx';
+import ThumbnailScroll from './ThumbnailScroll.jsx'
 
 class ImagePreview extends React.Component {
   constructor(props) {
@@ -10,29 +12,83 @@ class ImagePreview extends React.Component {
     this.state = {
       selectedIndex: 0,
       expanded: false,
+      hidePrev: true,
+      hideNext: false,
     };
     this.toggleModal = this.toggleModal.bind(this);
+    this.selectPrev = this.selectPrev.bind(this);
+    this.selectNext = this.selectNext.bind(this);
+    this.arrowKeyHandler = this.arrowKeyHandler.bind(this)
   }
 
   toggleModal() {
     this.setState((state) => ({ expanded: !state.expanded }));
+    console.log('Modal selected');
+  }
+
+  selectNext() {
+    const { selectedIndex, hideNext } = this.state;
+    const { photos } = this.props;
+    this.setState({ hidePrev: false });
+    if (hideNext) {
+      return;
+    }
+    if (selectedIndex === photos.length - 2) {
+      this.setState({ hideNext: true });
+    }
+    this.setState((state) => ({ selectedIndex: state.selectedIndex + 1 }));
+  }
+
+  selectPrev() {
+    const { selectedIndex, hidePrev } = this.state;
+    const { photos } = this.props;
+    this.setState({ hideNext: false });
+    if (hidePrev) {
+      return;
+    }
+    if (selectedIndex === 1) {
+      this.setState({ hidePrev: true });
+    }
+    this.setState((state) => ({ selectedIndex: state.selectedIndex - 1 }));
+  }
+
+  arrowKeyHandler(e) {
+    if (e.keyCode === 39) {
+      this.selectNext();
+    }
+    if (e.keyCode === 37) {
+      this.selectPrev();
+    }
+    console.log('Keypress')
   }
 
   render() {
     const { photos } = this.props;
-    const { selectedIndex, expanded } = this.state;
+    const { selectedIndex, expanded, hideNext, hidePrev } = this.state;
     if (photos) {
       return (
         <>
-          <div className='imagePreviewContainer' onClick={this.toggleModal}>
-            <ImageViewBox photos={photos} selectedIndex={selectedIndex} />
+          <div className='imagePreviewContainer' onKeyDown={this.arrowKeyHandler} tabIndex='0' >
+            <>
+              <div className='imageBox'>
+                <div className='selectPrevImage' hidden={hidePrev} onClick={this.selectPrev}>
+                  {`<`}
+                </div>
+                <div
+                  className='selectNextImage'
+                  hidden={hideNext}
+                  onClick={this.selectNext}
+                >{`>`}</div>
+                <img
+                  src={photos[selectedIndex].url}
+                  alt=''
+                  className='selectedImage'
+                  onClick={this.toggleModal}
+                />
+              </div>
+              <ThumbnailScroll/>
+            </>
           </div>
-          <ImageModal
-            toggleModal={this.toggleModal}
-            expanded={expanded}
-            photos={photos}
-            selectedIndex={selectedIndex}
-          />
         </>
       );
     }
